cb7f2f15dcdff70f73efae16e4b7c18b1d85ed86
Divity <73408742+Divity@users.noreply.github.com>
2025-11-07 02:43:19 -0500

    Add imgui.lua

---
+++ deps/premake/imgui.lua
@@ -0,0 +1,40 @@
+imgui = {
+	source = path.join(dependencies.basePath, "imgui"),
+	backends = path.join(dependencies.basePath, "imgui/backends"),
+}
+
+function imgui.import()
+	links { "imgui" }
+	imgui.includes()
+end
+
+function imgui.includes()
+	includedirs {
+		imgui.source,
+		imgui.backends,
+	}
+end
+
+function imgui.project()
+	project "imgui"
+		language "C++"
+		warnings "Off"
+		kind "StaticLib"
+
+		imgui.includes()
+
+		files {
+			path.join(imgui.source, "*.cpp"),
+			path.join(imgui.source, "*.h"),
+			path.join(imgui.backends, "imgui_impl_win32.cpp"),
+			path.join(imgui.backends, "imgui_impl_win32.h"),
+			path.join(imgui.backends, "imgui_impl_dx11.cpp"),
+			path.join(imgui.backends, "imgui_impl_dx11.h"),
+		}
+
+		defines {
+			"IMGUI_IMPL_API=extern `"C`"",
+		}
+end
+
+table.insert(dependencies, imgui)
